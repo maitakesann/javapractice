3a41a2f6770e71efc5c91a5d7ba3f99c53396e78
maitakesann <kankore1204a@yahoo.co.jp>
2021-07-06 10:04:19 +0000

    [Add]User Post

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '725c97936ccc31277602ca9e8705327fab05b79f597547f2268c859f3d9bbdb0364eeaf7455452b1b8c293cce14564d8e0848295019b16e377f480966cbe4ffd'
