16d5dff7adc7a16a53e4d0b29f9f94728be6b796
maitakesann <kankore1204a@yahoo.co.jp>
2021-07-11 05:35:46 +0000

    [Add]user edit,error,権限付与

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-  end  
+  end
+  resources :users, only: [:show,:edit,:update]
 end
