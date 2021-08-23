3f17dc15a2aac8b3fdd15603a337fb235eaf3efa
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-23 01:37:23 +0000

    root ADD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   get 'relationships/create'
   get 'relationships/destroy'
-  devise_for :users
+  devise_for :users do
+  resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  
   root to: 'homes#top'
   get '/search', to: 'searchs#search'
+  
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
