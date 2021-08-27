f8b4bdae589a08ec003022471166e494841f6bf4
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-27 04:51:30 +0000

    Integer.parseInt

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ public class Main{
   public static void main (String[] args){
     int a = 3;
     int b = 5;
-    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+    int c = a * b;
+    System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    
+    String age = "31";
+    int n = Integer.parseInt (age);
+    System.out.println("あなたは来年" + (n + 1) + "歳になります");
   }
 }
