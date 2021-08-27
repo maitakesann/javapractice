2a51328635fde502d59d1f467ca82f8326555d93
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-27 02:16:12 +0000

    [Add]数列

--- hello.java
+++ hello.java
@@ -1,37 +1,58 @@
-public class hello {
 
-  public static void main(String []args) {
-    System.out.println("Hello, World!");
+package SequenceNumbers;
 
-    System.out.println("The sum of 2 and 3 is 5.");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    int x = 7 ;int y = 5;
-    System.out.println(x * y);
+public class Main {
 
-    int oddSum = 0;
-    int evenSum = 0;
+  public class hello {
+      public static void main(String args[]) {
+          task();
 
-    int[] numbers = {1,2,3,4,5,6,7,8,9};
-      for (int i = 0; i < numbers.length; i++){
-        if(numbers[i] % 2 == 0){
-          evenSum += numbers[i];
-        }else{
-          oddSum += numbers[i];
-        }
-      }
-    System.out.println(oddSum);
-    System.out.println(evenSum);
-    System.out.println(numbers[2]);
-
-    for (int i = 1; i <= 3; i++){
-      for (int j = 3; j >= 1; j--){
-        if (i != j){
-          System.out.println(i + "," + j);
-        }else{
+          // 計算の確認として、フィボナッチ数列を順番に表示 (20番目まで)
+          for (int i = 1; i <= 20; i++) {
+              System.out.print("" + calculation(i) + " ");
+          }
           System.out.println();
-        }
       }
-    }
 
+      static void task() {
+          System.out.println("フィボナッチ数列の第n項を調べてやる、です");
+          System.out.println("好きな数を打ちやがれ、です");
+          Scanner scan = new Scanner(System.in); // キー入力
+          try {
+              int n = scan.nextInt();
+              if (1 <= n) {
+                  print_result(calculation(n), n); // 計算結果表示
+              } else {
+                  System.out.println("1 以上で指定してください");
+              }
+          } catch (InputMismatchException e) { // 入力が数字以外の場合
+              System.out.println("数字入れろって言ってるダルォ！？");
+          }
+      }
+
+      static long calculation(int n) {
+          if (n < 2) {
+              return 0;
+          }
+          if (n == 2) {
+              return 1;
+          }
+          long ans = 0;
+          long n1 = 0;
+          long n2 = 1;
+          for (n -= 2; n > 0; n--) {
+              ans = n1 + n2;
+              n1 = n2;
+              n2 = ans;
+          }
+          return ans;
+      }
+
+      static void print_result(long fib, int n) {
+          System.out.println("フィボナッチ数列の第" + n + "項目の数は" + fib + "です。");
+      }
   }
 }
