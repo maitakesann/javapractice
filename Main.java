f0c5f62048d042e97df646a08d24e5754f391694
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-27 04:58:56 +0000

    java Random

--- Main.java
+++ Main.java
@@ -4,9 +4,12 @@ public class Main{
     int b = 5;
     int c = a * b;
     System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
-    
+
     String age = "31";
     int n = Integer.parseInt (age);
     System.out.println("あなたは来年" + (n + 1) + "歳になります");
+
+    int r = new java.util.Random() .nextInt(90);
+    System.out.println("あなたは多分" + r + "歳ですね？");
   }
 }
