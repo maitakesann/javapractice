3ffea4742e56207e0659020d826c07a8d60f2c5c
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-28 05:37:20 +0000

    Update 数当てゲーム

--- Java3.java
+++ Java3.java
@@ -1,8 +1,8 @@
 class Java3{
     public static void main(String[] args){
       System.out.println("数当てゲーム");
-      int ans = new java.util.Random().nextInt(9);
-      for (int i = 1; i < 5; i++){
+      int ans = new java.util.Random().nextInt(10);
+      for (int i = 0; i < 5; i++){
         System.out.println("0~9の数字を入力してください");
         int num = new java.util.Scanner(System.in).nextInt();
           if(ans == num){
