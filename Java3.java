9ebc0cb0fa61a956a2825b60cf00cab54a2b8776
maitakesann <kankore1204a@yahoo.co.jp>
2021-08-29 02:55:03 +0000

    数当てゲームrepeat

--- Java3.java
+++ Java3.java
@@ -1,11 +1,27 @@
 class Java3{
     public static void main(String[] args){
+
+      int isHungry = 0 ;
+      String food = "rice";
+      System.out.println("こんにちは");
+      if (isHungry == 0){
+        System.out.println("お腹がいっぱいです");
+      }else{
+        System.out.println("腹ペコです");
+      }
+      if(isHungry == 0){
+        System.out.println(food + "をいただきます");
+      }else{
+        System.out.println("ごちそうさまでした");
+      }
+
+
       System.out.println("数当てゲーム");
       int ans = new java.util.Random().nextInt(10);
-      for (int i = 0; i < 5; i++){
-        System.out.println("0~9の数字を入力してください");
-        int num = new java.util.Scanner(System.in).nextInt();
-          if(ans == num){
+      for(int i = 0; i < 5; i++){
+        System.out.println("0~9の数字を入れてください");
+        int num = new java.util.Scanner(System.in).nextInt(10);
+          if(num == ans){
             System.out.println("アタリ！");
             break;
           }else{
